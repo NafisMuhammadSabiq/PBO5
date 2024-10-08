5cc63f8df74132d31110a7e3a07050f4dbbcd102
NafisMuhammadSabiq <nafisharist@gmail.com>
2024-10-08 09:53:21 +0700

    PBO

--- pewarisan.js
+++ pewarisan.js
@@ -2,13 +2,15 @@ class Orang {
     constructor(name, kelas) {
         this.name = name;
         this.kelas = kelas;
+        this.lulusan = this.lulusan;
     }
-
-    tampilkanInformasi() {
+    tampilkanMahasiswa() {
         console.log(`Nama: ${this.name}, Kelas: ${this.kelas}`);
     }
+    tampilkanMaba() {
+        console.log(`Nama: ${this.name}, alamat: ${this.alamat}`);
+    }
 }
-
 // Definisi kelas turunan
 class Mahasiswa extends Orang {
     constructor(name, kelas, nim) {
@@ -16,12 +18,27 @@ class Mahasiswa extends Orang {
         this.nim = nim;
     }
 
-    tampilkanInformasi() {
-        super.tampilkanInformasi(); // Memanggil metode dari kelas induk
+    tampilkanMahasiswa() {
+        super.tampilkanMahasiswa(); // Memanggil metode dari kelas induk
         console.log(`NIM: ${this.nim}`);
     }
 }
-
 // Penggunaan kelas turunan
 const mahasiswa1 = new Mahasiswa("Anwar", "IP231", "123456");
-mahasiswa1.tampilkanInformasi();
+mahasiswa1.tampilkanMahasiswa();
+
+// Definisi kelas turunan
+class Maba extends Orang {
+    constructor(name, lulusan, alamat) {
+        super(name, lulusan); // Memanggil konstruktor kelas induk
+        this.alamat = alamat;
+    }
+
+    tampilkanMaba() {
+        super.tampilkanMaba(); // Memanggil metode dari kelas induk
+        console.log(`Alamat: ${this.alamat}`);
+    }
+}
+// Penggunaan kelas turunan
+const maba1 = new Maba("Joni", "SMA 6 Bekasi", "Bekasi");
+maba1.tampilkanMaba();
--- polimorfisme.js
+++ polimorfisme.js
@@ -1,28 +1,36 @@
-// Definisi kelas induk
 class Orang {
     tampilkanPeran() {
         console.log("Peran: Orang biasa");
     }
 }
-
-// Definisi kelas turunan
 class Siswa extends Orang {
     tampilkanPeran() {
         console.log("Peran: Siswa");
     }
 }
-
 class Guru extends Orang {
     tampilkanPeran() {
         console.log("Peran: Guru");
     }
 }
-
+class Security extends Orang {
+    tampilkanPeran() {
+        console.log("Peran: Security");
+    }
+}
+class WaliMurid extends Orang {
+    tampilkanPeran() {
+        console.log("Peran: Wali Murid");
+    }
+}
 // Menggunakan polimorfisme
 const orang1 = new Orang();
 const siswa1 = new Siswa();
 const guru1 = new Guru();
-
+const security1 = new Security();
+const waliMurid1 = new WaliMurid();
 orang1.tampilkanPeran();    // Output: Peran: Orang biasa
 siswa1.tampilkanPeran(); // Output: Peran: Siswa
 guru1.tampilkanPeran();     // Output: Peran: Guru
+security1.tampilkanPeran(); // Output: Peran: Security
+waliMurid1.tampilkanPeran(); // Output: Peran: Wali Murid
